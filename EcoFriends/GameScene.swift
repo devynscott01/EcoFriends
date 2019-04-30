5e74c481c2fcf35d6305ff6e9d52701d46ebe57c
ZIA SCOTT <zia.scott19@clcmbp14.local>
2019-04-30 08:53:04 -0500

    wrote code in line 42

--- EcoFriends/GameScene.swift
+++ EcoFriends/GameScene.swift
@@ -8,82 +8,67 @@
 
 import SpriteKit
 import GameplayKit
+import UIKit
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let trashPiece: UInt32 = 0b1
+}
+
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var background = SKSpriteNode(imageNamed: "sea")
+    
+    
     
     override func didMove(to view: SKView) {
+        background.scale(to: CGSize.init(width: 1334, height: 750))
+        background.zPosition = 0
+        background.position = CGPoint(x: self.size.width / 40, y: self.size.height / 40)
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = CGVector.zero
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody = border
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+       
+    addChild(background)
+    
+        run(SKAction.repeatForever(SKAction.sequence([SKAction.run(trash), SKAction.wait(forDuration: 1.0)])))
+      }
+    
+    
+    
+    // >:) hi
+
+
+    
+    func trash () {
+        let trashPiece = SKSpriteNode(imageNamed: "cup")
+        let actualY = random(min: trashPiece.size.height/2, max: size.height - trashPiece.size.height/2)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        trashPiece.position = CGPoint(x: size.width + trashPiece.size.width/2, y: actualY)
+        trashPiece.scale(to: CGSize(width: 35, height: 35))
+        trashPiece.physicsBody = SKPhysicsBody(rectangleOf: trashPiece.size)
+        trashPiece.physicsBody?.affectedByGravity = false
+        trashPiece.physicsBody?.friction = 0
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+        addChild(trashPiece)
+        
+        
+        let actualDuration = CGFloat.random(in: 1.0...10.0)
+        let actionMove = SKAction.move(to: CGPoint(x: 100, y: actualY), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        trashPiece.run(SKAction.sequence([actionMove, actionMoveDone]))
+        
+        
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max-min) + min
     }
 }
+
